a1484c188662fff8f1c663a40559372fbc61aef7
experionjoel <joel.philip@experionglobal.com>
2016-10-27 13:25:44 +0530

    second commit

--- factorial.java
+++ factorial.java
@@ -1,4 +1,5 @@
 // Program to find factorial of a number
+import java.io.*;
 
 class Factoral
 {
@@ -6,7 +7,7 @@ class Factoral
 	{
 		int i, j, f=1, n = 5;
 		System.out.println("Enter limit: ");
-		int num = Integer.parseInt(System.console.readLine());
+		int num = Integer.parseInt(System.console().readLine());
 		for(i=1;i<=n;i++)
 		{ f = f * i;}
 		System.out.println("factorial of "+n+" is "+f);
